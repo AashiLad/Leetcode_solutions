1bb3e15f93c36e7070d2fdc49b24b37041f42d57
Aashi Lad <aashilad22@gmail.com>
2026-07-15 09:41:45 +0530

    Time: 18 ms (83.95%) | Memory: 17.4 MB (55.60%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -6,26 +6,32 @@ public:
 
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n - 3; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j < n - 2; j++){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
 
-            for(int j = i+1; j < n;){
-                int p = j+1, q = n-1;
+                int p = j+1;
+                int q = n-1;
+
                 while(p < q){
-                    long long sum = (long long)nums[i] + (long long)nums[j] +
-                                        (long long)nums[p] +(long   long)nums[q];
-                    if(sum < target){
-                        p++;
-                    } else if(sum > target){
-                        q--;
-                    } else{
+                    long long sum = (long long)nums[i] +(long long)nums[j] +(long long)nums[p] +(long long)nums[q];
+                    if(sum == target){
                         ans.push_back({nums[i], nums[j], nums[p], nums[q]});
-                        p++, q--;
+                        p++;
+                        q--;
+
                         while(p < q && nums[p] == nums[p-1]) p++;
+                        while(p < q && nums[q] == nums[q+1]) q--;
                     }
-                }
-                j++;
-                while(j < n && nums[j] == nums[j-1]) j++;
+                    else if(sum > target){
+                        q--;
+                    } 
+                    else{
+                        p++;
+                    }
+
+                } 
             }
         }
         return ans;
