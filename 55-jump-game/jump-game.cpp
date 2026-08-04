53ade94d3d0e400b245a23daf57a202cfb797023
Aashi Lad <aashilad22@gmail.com>
2026-08-04 16:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (58.26%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxidx = 0;
+
+        for(int i = 0; i < n-1; i++){
+            if(i > maxidx) return false;
+
+            maxidx = max(maxidx, i+nums[i]);
+        }
+        return maxidx >= n-1;
+        
+    }
+};
